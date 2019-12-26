f44ebf2ebf7110e925d297193f83cfebfb364865
Joker <joker1095920109@gmail.com>
2019-12-26 20:03:31 +0800

    王凯 登录逻辑补充

--- mall_admin/src/main/java/com/cskaoyan/bean/BaseRespVo.java
+++ mall_admin/src/main/java/com/cskaoyan/bean/BaseRespVo.java
@@ -7,4 +7,31 @@ public class BaseRespVo<T> {
      T data;
      int errno;
      String errmsg;
+
+     public T getData() {
+          return data;
+     }
+
+     public BaseRespVo<T> setData(T data) {
+          this.data = data;
+          return this;
+     }
+
+     public int getErrno() {
+          return errno;
+     }
+
+     public BaseRespVo<T> setErrno(int errno) {
+          this.errno = errno;
+          return this;
+     }
+
+     public String getErrmsg() {
+          return errmsg;
+     }
+
+     public BaseRespVo<T> setErrmsg(String errmsg) {
+          this.errmsg = errmsg;
+          return this;
+     }
 }
