66c80315e1c943de72cb1de9556f87121d5d04c6
yuanchuan <380029172@qq.com>
2019-12-26 16:11:28 +0800

    By 袁川 添加了数据库依赖和配置了数据库，以及更改了包名

---
+++ mall_admin/src/main/java/com/cskaoyan/MallAdminApplication.java
@@ -0,0 +1,14 @@
+package com.cskaoyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class MallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+
+}
