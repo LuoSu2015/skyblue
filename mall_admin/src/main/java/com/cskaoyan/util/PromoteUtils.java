014cca79528069968d9c9080d0b5cebce4a8a35f
yuanchuan <380029172@qq.com>
2019-12-27 08:13:18 +0800

    完成了推广管理，第一个子模块能正常显示，正在做工具类

---
+++ mall_admin/src/main/java/com/cskaoyan/util/PromoteUtils.java
@@ -0,0 +1,38 @@
+package com.cskaoyan.util;
+
+import com.cskaoyan.bean.Ad;
+import com.cskaoyan.bean.BaseRespVo;
+import com.cskaoyan.bean.propate.PageWrapper;
+import com.github.pagehelper.PageHelper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PromoteUtils<T> {
+
+    public static BaseRespVo preSetBaseRespVo(PageWrapper pageWrapper) {
+
+        int limit = pageWrapper.getLimit();
+        String name = pageWrapper.getName();
+        String order = pageWrapper.getOrder();
+        int page = pageWrapper.getPage();
+        String sort = pageWrapper.getSort();
+        BaseRespVo adBaseRespVo = new BaseRespVo<>();
+        String orderBySort = sort + " " + order;
+        PageHelper.startPage(page, limit, orderBySort);
+
+        return adBaseRespVo;
+    }
+
+
+    public  BaseRespVo postSetBaseRespVo(BaseRespVo baseRespVo,List<T> object) {
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("items", object);
+        hashMap.put("total", object.size());
+        baseRespVo.setData(hashMap);
+        baseRespVo.setErrno(0);
+        baseRespVo.setErrmsg("成功");
+        return baseRespVo;
+    }
+}
