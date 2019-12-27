d2b9dcd2737cd9812b6e0be9a4e7e2bfe604cfb3
yuanchuan <380029172@qq.com>
2019-12-27 15:26:42 +0800

    已完成推广管理的所有查询，下一步完成添加或者修改

--- mall_admin/src/main/java/com/cskaoyan/controller/promote/PromoteController.java
+++ mall_admin/src/main/java/com/cskaoyan/controller/promote/PromoteController.java
@@ -1,8 +1,6 @@
 package com.cskaoyan.controller.promote;
 
-import com.cskaoyan.bean.Ad;
-import com.cskaoyan.bean.BaseRespVo;
-import com.cskaoyan.bean.Storage;
+import com.cskaoyan.bean.*;
 import com.cskaoyan.bean.propate.PageWrapper;
 import com.cskaoyan.service.promote.PromoteService;
 import com.cskaoyan.util.PromoteUtils;
@@ -62,21 +60,64 @@ public class PromoteController {
 
     /* 广告展示 */
     @RequestMapping("admin/ad/list")
-    public BaseRespVo<List<Ad>> queryAdListByNameAnd(PageWrapper pageWrapper) {
+    public BaseRespVo<List<Ad>> queryAdListByNameAndContent(PageWrapper pageWrapper) {
 
         PromoteUtils<Ad> adPromoteUtils = new PromoteUtils<>();
         BaseRespVo baseRespVo = PromoteUtils.preSetBaseRespVo(pageWrapper);
-        List<Ad> ads = promoteService.queryAdListByName(pageWrapper.getName());
-         baseRespVo = adPromoteUtils.postSetBaseRespVo(baseRespVo, ads);
+        List<Ad> ads = promoteService.queryAdByNameAndContent(pageWrapper.getName(), pageWrapper.getContent());
+        baseRespVo = adPromoteUtils.postSetBaseRespVo(baseRespVo, ads);
 
         return baseRespVo;
     }
 
 
-    /*
-    *
-    *
-    * http://192.168.2.100:8081/admin/coupon/list?page=1&limit=20&sort=add_time&order=desc
-    * */
+    /*  优惠券 */
+    @RequestMapping("admin/coupon/list")
+    public BaseRespVo queryCouponListByNameAndTypeAndStatus(PageWrapper pageWrapper) {
 
+        PromoteUtils<Coupon> adPromoteUtils = new PromoteUtils<>();
+        BaseRespVo baseRespVo = PromoteUtils.preSetBaseRespVo(pageWrapper);
+        List<Coupon> couponList = promoteService.queryCouponListByNameAndTypeAndStatus(pageWrapper.getName(), pageWrapper.getType(), pageWrapper.getStatus());
+        baseRespVo = adPromoteUtils.postSetBaseRespVo(baseRespVo, couponList);
+
+        return baseRespVo;
+    }
+
+    /*  专题管理  */
+    /*admin/topic/list title=sfdf&subtitle=dasd*/
+    @RequestMapping("admin/topic/list")
+    public BaseRespVo queryTopicListByTitleAndSubtitle(PageWrapper pageWrapper) {
+        PromoteUtils<Topic> adPromoteUtils = new PromoteUtils<>();
+        BaseRespVo baseRespVo = PromoteUtils.preSetBaseRespVo(pageWrapper);
+        List<Topic> topicList = promoteService.queryTopicListByTitleAndSubtitle(pageWrapper.getTitle(), pageWrapper.getSubtitle());
+        baseRespVo = adPromoteUtils.postSetBaseRespVo(baseRespVo, topicList);
+
+        return baseRespVo;
+
+    }
+
+    /*  团购规则 */
+//      admin/groupon/list?goodsId=
+    @RequestMapping("admin/groupon/list")
+    public BaseRespVo queryGrouponRulesListByGoodsId(PageWrapper pageWrapper) {
+        PromoteUtils<Groupon_rules> adPromoteUtils = new PromoteUtils<>();
+        BaseRespVo baseRespVo = PromoteUtils.preSetBaseRespVo(pageWrapper);
+        List<Groupon_rules> groupon_rulesList = promoteService.queryGrouponRulesListByGoodsId(pageWrapper.getGoodsId());
+        baseRespVo = adPromoteUtils.postSetBaseRespVo(baseRespVo, groupon_rulesList);
+
+        return baseRespVo;
+
+    }
+    /*团购活动*/
+//     admin/groupon/listRecord?page=1&limit=20&sort=add_time&order=desc  grouponrules
+    @RequestMapping("admin/groupon/listRecord")
+    public BaseRespVo queryGrouponListRecordByGoodsId(PageWrapper pageWrapper){
+        PromoteUtils<Groupon> adPromoteUtils = new PromoteUtils<>();
+        BaseRespVo baseRespVo = PromoteUtils.preSetBaseRespVo(pageWrapper);
+        List<Groupon> grouponList = promoteService.queryGrouponListRecordByGoodsId(pageWrapper.getGoodsId());
+        baseRespVo = adPromoteUtils.postSetBaseRespVo(baseRespVo, grouponList);
+
+        return baseRespVo;
+
+    }
 }
